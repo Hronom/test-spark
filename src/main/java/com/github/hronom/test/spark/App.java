7beefd7e4cac5dc19754323cb5e3f179e1f071a9
Hronom <hronom@gmail.com>
2016-03-31 18:04:16 +0300

    Initial commit

---
+++ src/main/java/com/github/hronom/test/spark/App.java
@@ -0,0 +1,10 @@
+package com.github.hronom.test.spark;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
